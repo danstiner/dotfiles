ab122d32c1cccdeeb159851e6ee242453e2576f6
Daniel Stiner <danstiner@gmail.com>
2016-09-18 12:03:34 -0700

    Install latest version of ansible

--- ubuntu.sh
+++ ubuntu.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+sudo apt-get install -y software-properties-common
+sudo apt-add-repository -y ppa:ansible/ansible
+sudo apt-get update
 sudo apt-get install -y ansible
 
 ansible-playbook "${BASH_SOURCE%/*}/playbook.yaml"
