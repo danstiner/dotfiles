9d8817a821b62193033a0db1c3cbcd2643ed7664
Daniel Stiner <danstiner@gmail.com>
2016-09-27 22:06:13 -0700

    Exit if any setup command fails in script

--- ubuntu.sh
+++ ubuntu.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-sudo apt-get install -y software-properties-common
-sudo apt-add-repository -y ppa:ansible/ansible
-sudo apt-get update
-sudo apt-get install -y ansible
+sudo apt-get install -y software-properties-common || exit 1
+sudo apt-add-repository -y ppa:ansible/ansible || exit 1
+sudo apt-get update || exit 1
+sudo apt-get install -y ansible || exit 1
 
-ansible-playbook "${BASH_SOURCE%/*}/playbook.yaml"
+ansible-playbook "${BASH_SOURCE%/*}/playbook.yaml" || exit 1
