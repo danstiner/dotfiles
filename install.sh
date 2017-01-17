74b1b33a72082c864292199e1ec1c27b2b0281c0
Daniel Stiner <danstiner@gmail.com>
2017-01-17 15:20:09 -0800

    Skip installing ansible if it is already present

--- install.sh
+++ install.sh
@@ -3,14 +3,16 @@
 if [ -d "${BASH_SOURCE%/*}/.git" ]; then
   # Running from a clone of the repository
 
-  # Install ansible
-  if [ -f "/etc/fedora-release" ]; then
-    sudo dnf -y install ansible python2-dnf || exit 1
-  else
-    sudo apt-get install -y software-properties-common || exit 1
-    sudo apt-add-repository -y ppa:ansible/ansible || exit 1
-    sudo apt-get update || exit 1
-    sudo apt-get install -y ansible || exit 1
+  # Install ansible only if not already present
+  if ! hash ansible-playbook 2>/dev/null; then
+    if [ -f "/etc/fedora-release" ]; then
+      sudo dnf -y install ansible python2-dnf || exit 1
+    else
+      sudo apt-get install -y software-properties-common || exit 1
+      sudo apt-add-repository -y ppa:ansible/ansible || exit 1
+      sudo apt-get update || exit 1
+      sudo apt-get install -y ansible || exit 1
+    fi
   fi
 
   # Run playbook
