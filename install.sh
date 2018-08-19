56217aa288e17774cf47762cc2f4adceffd83ddf
Daniel Stiner <danstiner@gmail.com>
2018-08-18 21:56:00 -0700

    Safer install script with set -euxo pipefail

--- install.sh
+++ install.sh
@@ -1,43 +1,45 @@
 #!/bin/bash
 
+set -euxo pipefail
+
 if [ -d "${BASH_SOURCE%/*}/.git" ]; then
   # Running from a clone of the repository
 
   # Install ansible only if not already present
   if ! hash ansible-playbook 2>/dev/null; then
     if [ -f "/etc/fedora-release" ]; then
-      sudo dnf -y install ansible python2-dnf || exit 1
+      sudo dnf -y install ansible python2-dnf
     else
-      sudo apt-get install -y software-properties-common || exit 1
-      sudo apt-add-repository -y ppa:ansible/ansible || exit 1
-      sudo apt-get update || exit 1
-      sudo apt-get install -y ansible || exit 1
+      sudo apt-get install -y software-properties-common
+      sudo apt-add-repository -y ppa:ansible/ansible
+      sudo apt-get update
+      sudo apt-get install -y ansible
     fi
   fi
 
   # Install ansible galaxy roles
-  ansible-galaxy install -r requirements.yml || exit 1
+  ansible-galaxy install -r requirements.yml
 
   # Run playbook
-  ansible-playbook "${BASH_SOURCE%/*}/playbook.yaml" || exit 1
+  ansible-playbook "${BASH_SOURCE%/*}/playbook.yaml" 
 
 else
   # Running from outside repository, first make a clone
 
   # Install git
   if [ -f "/etc/fedora-release" ]; then
-    sudo dnf install git || exit 1
+    sudo dnf install git
   else
-    sudo apt install git || exit 1
+    sudo apt install git
   fi
 
   # Clone the repository
   if [ ! -f "$HOME/dotfiles/install.sh" ]; then
-    git clone https://github.com/danstiner/dotfiles.git "$HOME/dotfiles" || exit 1
+    git clone https://github.com/danstiner/dotfiles.git "$HOME/dotfiles"
   fi
 
   # Run installer from clone
-  "$HOME/dotfiles/install.sh" || exit 1
+  "$HOME/dotfiles/install.sh"
 
 fi
 
